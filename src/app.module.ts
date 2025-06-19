69c7da236b75d00712976e43d1ad46ead13af06e
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-06-19 09:50:16 -0600

    feat: configure environment variables and add config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
+import config from './config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      load: [config],
+      isGlobal: true,
+      validationSchema: Joi.object({
+        CLUSTER: Joi.string().required(),
+        MONGO_USER: Joi.string().required(),
+        MONGO_PWD: Joi.string().required(),
+        MONGO_DB_NAME: Joi.string().required(),
+        MONGO_CONNECTION: Joi.string().required(),
+        NODE_ENV: Joi.string().required(),
+      }),
+    }),
+    DatabaseModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
