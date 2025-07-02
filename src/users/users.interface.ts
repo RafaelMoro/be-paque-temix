cab627734866d52c857b83275204529088e9c5c8
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-07-01 23:43:16 -0600

    feat: implement user creation logic with email verification and password hashing

--- src/users/users.interface.ts
+++ src/users/users.interface.ts
@@ -1,1 +1,15 @@
+import { CreateUserDto } from './dtos/users.dto';
+
 export type Role = 'admin' | 'user';
+
+export interface CreateUserProps {
+  data: CreateUserDto;
+  isAdmin?: boolean;
+}
+
+export interface CreateUserResponse {
+  email: string;
+  name: string;
+  lastName: string;
+  role: Role[];
+}
