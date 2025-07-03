d3acbf1a74622509c5e384c3beeadd0583602dd6
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-07-03 16:37:09 -0600

    feat: add AuthController with local login functionality and cookie handling

---
+++ src/app.constant.ts
@@ -0,0 +1,3 @@
+export const DEV_ENV = 'development';
+export const PROD_ENV = 'production';
+export const ACCESS_TOKEN_COOKIE_NAME = 'accessToken';
---
+++ src/auth/controllers/auth.controller.ts
@@ -0,0 +1,33 @@
+import { Controller, Post, Req, Res, UseGuards } from '@nestjs/common';
+import { Request, Response } from 'express';
+import { AuthGuard } from '@nestjs/passport';
+
+import { AuthService } from '../services/auth.service';
+import { User } from '@/users/entities/users.entity';
+import { LOCAL_STRATEGY } from '../auth.constant';
+import { ACCESS_TOKEN_COOKIE_NAME, PROD_ENV } from '@/app.constant';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @UseGuards(AuthGuard(LOCAL_STRATEGY))
+  @Post('local')
+  loginLocal(
+    @Req() request: Request,
+    @Res({ passthrough: true }) response: Response,
+  ) {
+    const user = request.user as User;
+
+    const res = this.authService.generateJWTAuth(user);
+    response.cookie(ACCESS_TOKEN_COOKIE_NAME, res.accessToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === PROD_ENV,
+      sameSite: 'strict',
+      maxAge: 1000 * 60 * 60 * 24 * 5, // 5 days
+    });
+    return {
+      user: res.user,
+    };
+  }
+}
