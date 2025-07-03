66a7a27782c135c4158c8b1701752a5fe15f1b10
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-07-02 21:15:13 -0600

    feat: implement user creation endpoint in UsersController and configure Mongoose module in UsersModule

--- src/users/controllers/users.controller.ts
+++ src/users/controllers/users.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { UsersService } from '../services/users.service';
+import { CreateUserDto } from '../dtos/users.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private userService: UsersService) {}
+
+  // @Public()
+  @Post()
+  createUser(@Body() payload: CreateUserDto) {
+    return this.userService.createUser({ data: payload });
+  }
+}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './controllers/users.controller';
 import { UsersService } from './services/users.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UsersSchema } from './entities/users.entity';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: User.name,
+        schema: UsersSchema,
+      },
+    ]),
+  ],
   controllers: [UsersController],
   providers: [UsersService],
 })
