e1deefaa8fb46916b3aa9e646fc06ac0a88c89e4
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-07-02 21:40:32 -0600

    feat: implement JWT guard and public access decorator for user creation

--- src/users/controllers/users.controller.ts
+++ src/users/controllers/users.controller.ts
@@ -7,6 +7,7 @@ import {
 } from '@nestjs/common';
 import { UsersService } from '../services/users.service';
 import { CreateUserDto } from '../dtos/users.dto';
+import { Public } from '@/auth/decorators/public/public.decorator';
 
 @Controller('users')
 export class UsersController {
@@ -17,7 +18,7 @@ export class UsersController {
   //   return this.userService.findByEmail(email);
   // }
 
-  // @Public()
+  @Public()
   @Post()
   createUser(@Body() payload: CreateUserDto) {
     return this.userService.createUser({ data: payload });
