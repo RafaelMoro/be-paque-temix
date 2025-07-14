7624f8b054ebac74c101c10b15c64c8ea8a02353
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-07-13 22:27:22 -0600

    feat: add ApiSchema decorator to LoginDto for Swagger documentation

--- src/auth/dtos/auth.dtos.ts
+++ src/auth/dtos/auth.dtos.ts
@@ -1,6 +1,7 @@
-import { ApiProperty } from '@nestjs/swagger';
+import { ApiProperty, ApiSchema } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
+@ApiSchema({ name: 'LoginDto' })
 export class LoginDto {
   @ApiProperty()
   @IsString()
