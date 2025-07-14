8715ff4571922356ff24598528914d79e8ee8868
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-07-13 20:48:09 -0600

    feat: add Swagger UI integration and update dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import * as cookieParser from 'cookie-parser';
+import { ValidationPipe } from '@nestjs/common/pipes';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 import { GeneralAppExceptionFilter } from './exceptions/GeneralException.filter';
-import { ValidationPipe } from '@nestjs/common/pipes';
+import { VERSION_RESPONSE } from './app.constant';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Kraft envios API')
+    .setDescription('This API is connected to mongoDB and uses and REST')
+    .setVersion(VERSION_RESPONSE ?? '1.0.0')
+    .addTag('videogames')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   app.use(cookieParser());
   app.useGlobalPipes(
     new ValidationPipe({
