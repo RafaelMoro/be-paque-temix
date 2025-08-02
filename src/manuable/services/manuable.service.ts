e10422c8c26747750a8ed17caf62720610362ef2
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-08-01 21:26:56 -0600

    feat: implement ManuableService with getManuableQuote method and error handling

--- src/manuable/services/manuable.service.ts
+++ src/manuable/services/manuable.service.ts
@@ -1,4 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Inject, Injectable } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
+
+import config from '@/config';
 
 @Injectable()
-export class ManuableService {}
+export class ManuableService {
+  constructor(
+    @Inject(config.KEY) private configService: ConfigType<typeof config>,
+  ) {}
+
+  async getManuableQuote() {
+    try {
+      // something
+    } catch (error) {
+      if (error instanceof Error) {
+        throw new BadRequestException(error.message);
+      }
+      throw new BadRequestException('An unknown error occurred');
+    }
+  }
+}
