f00400f3c5521ae41e8b12e7f652cda84c9d29f5
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-08-02 14:50:53 -0600

    feat: implement GeneralInfoDb entity and service with create and update methods

--- src/general-info-db/general-info-db.module.ts
+++ src/general-info-db/general-info-db.module.ts
@@ -1,7 +1,21 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { GeneralInfoDbService } from './services/general-info-db.service';
+import {
+  GeneralInfoDb,
+  GeneralInfoDbSchema,
+} from './entities/general-info-db.entity';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: GeneralInfoDb.name,
+        schema: GeneralInfoDbSchema,
+      },
+    ]),
+  ],
   providers: [GeneralInfoDbService],
   exports: [GeneralInfoDbService],
 })
--- src/general-info-db/services/general-info-db.service.ts
+++ src/general-info-db/services/general-info-db.service.ts
@@ -1,4 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
+import {
+  GeneralInfoDb,
+  GeneralInfoDbDoc,
+} from '../entities/general-info-db.entity';
 
 @Injectable()
-export class GeneralInfoDbService {}
+export class GeneralInfoDbService {
+  constructor(
+    @InjectModel(GeneralInfoDb.name)
+    private generalInfoDbModel: Model<GeneralInfoDb>,
+  ) {}
+
+  async createMnTk(tk: string) {
+    try {
+      const newMbTk = new this.generalInfoDbModel({ mnTk: tk });
+      const modelSaved: GeneralInfoDbDoc = await newMbTk.save();
+      return modelSaved;
+    } catch (error) {
+      if (error instanceof Error) {
+        throw new BadRequestException(error.message);
+      }
+      throw new BadRequestException('An unknown error occurred');
+    }
+  }
+
+  async updateMbTk() {
+    try {
+      // something
+    } catch (error) {
+      if (error instanceof Error) {
+        throw new BadRequestException(error.message);
+      }
+      throw new BadRequestException('An unknown error occurred');
+    }
+  }
+}
