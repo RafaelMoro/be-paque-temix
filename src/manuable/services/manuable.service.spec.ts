8f5e4337ebedbe3fd3bcb81e64078df7104e4492
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-08-08 16:46:47 -0600

    test: enhance ManuableService tests to validate getManuableSession functionality

--- src/manuable/services/manuable.service.spec.ts
+++ src/manuable/services/manuable.service.spec.ts
@@ -1,18 +1,47 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ManuableService } from './manuable.service';
+import config from '@/config';
+import axios from 'axios';
+import { GeneralInfoDbService } from '@/general-info-db/services/general-info-db.service';
 
 describe('ManuableService', () => {
   let service: ManuableService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ManuableService],
+      providers: [
+        ManuableService,
+        {
+          provide: config.KEY,
+          useValue: {
+            manuable: {
+              email: 'user@example.com',
+              pwd: 's3cr3t',
+              uri: 'https://mn.example.com',
+            },
+          },
+        },
+        {
+          provide: GeneralInfoDbService,
+          useValue: {
+            // not used in getManuableSession tests
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<ManuableService>(ManuableService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('getManuableSession returns token', async () => {
+    const token = 'mn-fake-token';
+    const postSpy = jest
+      .spyOn(axios, 'post')
+      .mockResolvedValueOnce({ data: { token } } as any);
+
+    const res = await service.getManuableSession();
+
+    expect(res).toBe(token);
+    expect(postSpy).toHaveBeenCalledTimes(1);
   });
 });
