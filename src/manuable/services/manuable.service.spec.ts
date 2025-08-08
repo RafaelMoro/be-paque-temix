17850ee581b4f1f3c0d967ef69dc4424fda95257
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-08-08 16:49:13 -0600

    test: add unit test for formatManuablePayload method in ManuableService

--- src/manuable/services/manuable.service.spec.ts
+++ src/manuable/services/manuable.service.spec.ts
@@ -3,6 +3,9 @@ import { ManuableService } from './manuable.service';
 import config from '@/config';
 import axios from 'axios';
 import { GeneralInfoDbService } from '@/general-info-db/services/general-info-db.service';
+import * as utils from '../manuable.utils';
+import { ManuablePayload } from '../manuable.interface';
+import { GetQuoteDto } from '@/app.dto';
 
 describe('ManuableService', () => {
   let service: ManuableService;
@@ -43,5 +46,41 @@ describe('ManuableService', () => {
 
     expect(res).toBe(token);
     expect(postSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('formatManuablePayload delegates to util and returns its result', () => {
+    const payload: GetQuoteDto = {
+      originPostalCode: '01010',
+      destinationPostalCode: '02020',
+      height: 10,
+      length: 20,
+      width: 30,
+      weight: 4.5,
+    };
+    const formatted: ManuablePayload = {
+      address_from: { country_code: 'MX', zip_code: '01010' },
+      address_to: { country_code: 'MX', zip_code: '02020' },
+      parcel: {
+        currency: 'MXN',
+        distance_unit: 'CM',
+        mass_unit: 'KG',
+        height: 10,
+        length: 20,
+        width: 30,
+        weight: 4.5,
+        product_id: 'PID',
+        product_value: 100,
+        quantity_products: 1,
+        content: 'Test',
+      },
+    };
+    const spy = jest
+      .spyOn(utils, 'formatPayloadManuable')
+      .mockReturnValueOnce(formatted);
+
+    const res = service.formatManuablePayload(payload);
+
+    expect(spy).toHaveBeenCalledWith(payload);
+    expect(res).toBe(formatted);
   });
 });
