5cbcef1d08273f9e531e53e2907adb872821156e
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-08-08 15:36:34 -0600

    test: enhance GuiaEnviaService tests to validate quote retrieval functionality

--- src/guia-envia/services/guia-envia.service.spec.ts
+++ src/guia-envia/services/guia-envia.service.spec.ts
@@ -1,5 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { GuiaEnviaService } from './guia-envia.service';
+import { GetQuoteData } from '@/global.interface';
+import { GetQuoteDto } from '@/app.dto';
 
 describe('GuiaEnviaService', () => {
   let service: GuiaEnviaService;
@@ -12,7 +14,29 @@ describe('GuiaEnviaService', () => {
     service = module.get<GuiaEnviaService>(GuiaEnviaService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('get quotes from GE', async () => {
+    const result: GetQuoteData[] = [
+      {
+        id: '1',
+        service: 'Estafeta Terreste',
+        total: 126.9,
+        source: 'GE',
+      },
+    ];
+    const payload: GetQuoteDto = {
+      originPostalCode: '00001',
+      destinationPostalCode: '00004',
+      height: 30,
+      length: 20,
+      width: 20,
+      weight: 2,
+    };
+
+    jest
+      .spyOn(service, 'getQuote')
+      // eslint-disable-next-line @typescript-eslint/require-await
+      .mockImplementation(async () => result);
+    const response = await service.getQuote(payload);
+    expect(response).toBe(result);
   });
 });
