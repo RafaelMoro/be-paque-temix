0142800bad22b84563a17408f7c35ddf6326c934
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-08-19 19:41:27 -0600

    feat: add service type determination for GE quotes

--- src/guia-envia/guia-envia.utils.ts
+++ src/guia-envia/guia-envia.utils.ts
@@ -1,13 +1,23 @@
 import { GetQuoteDto } from '@/app.dto';
 import { GEQuote } from './guia-envia.interface';
 import { GetQuoteGEDto } from './dtos/guia-envia.dtos';
-import { GetQuoteData } from '@/global.interface';
+import { GetQuoteData, QuoteTypeSevice } from '@/global.interface';
+
+const NEXT_DAY_REGEX = /^.expres.$/i;
+const STANDARD_REGEX = /^.terrestre.$/i;
+
+export const getTypeServiceGe = (service: string): QuoteTypeSevice | null => {
+  if (NEXT_DAY_REGEX.test(service)) return 'nextDay';
+  if (STANDARD_REGEX.test(service)) return 'standard';
+  return null;
+};
 
 export const formatQuotesGE = (quotes: GEQuote[]): GetQuoteData[] =>
   quotes.map((quote) => ({
     id: quote.id,
     service: quote.servicio,
     total: quote.total,
+    typeService: getTypeServiceGe(quote.servicio),
     source: 'GE',
   }));
 
