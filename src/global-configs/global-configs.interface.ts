b482cb9b181c81c8b3e21ae880a6e2298ab45930
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-08-23 10:28:57 -0600

    add ManageProfitMarginData and ManageProfitMarginResponse interfaces; enhance manageProfitMargin method with response structure

--- src/global-configs/global-configs.interface.ts
+++ src/global-configs/global-configs.interface.ts
@@ -1,1 +1,16 @@
+import { GeneralResponse } from '@/global.interface';
+
 export type TypeProfitMargin = 'percentage' | 'absolute';
+
+export interface ManageProfitMarginData {
+  value: number;
+  type: TypeProfitMargin;
+}
+
+export interface ManageProfitMarginResponse
+  extends Omit<GeneralResponse, 'data' | 'error'> {
+  error: null;
+  data: {
+    profitMargin: ManageProfitMarginData;
+  };
+}
--- src/global-configs/services/global-configs.service.ts
+++ src/global-configs/services/global-configs.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, Inject, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 
@@ -10,12 +10,16 @@ import {
   CreateGlobalConfigsDto,
   UpdateGlobalConfigsDto,
 } from '../dtos/global-configs.dto';
+import config from '@/config';
+import { ConfigType } from '@nestjs/config';
+import { ManageProfitMarginResponse } from '../global-configs.interface';
 
 @Injectable()
 export class GlobalConfigsService {
   constructor(
     @InjectModel(GlobalConfigs.name)
     private globalConfigModel: Model<GlobalConfigs>,
+    @Inject(config.KEY) private configService: ConfigType<typeof config>,
   ) {}
 
   async createProfitMargin(payload: CreateGlobalConfigsDto) {
@@ -33,7 +37,9 @@ export class GlobalConfigsService {
 
   async readProfitMargin() {
     try {
-      const profitMarginArray = await this.globalConfigModel.find().exec();
+      const profitMarginArray: GlobalConfigsDoc[] = await this.globalConfigModel
+        .find()
+        .exec();
       if (profitMarginArray.length === 0) {
         return null;
       }
@@ -50,7 +56,7 @@ export class GlobalConfigsService {
   async updateProfitMargin(changes: UpdateGlobalConfigsDto) {
     try {
       const { profitMarginId } = changes;
-      const updated = await this.globalConfigModel
+      const updated: GlobalConfigsDoc | null = await this.globalConfigModel
         .findByIdAndUpdate(profitMarginId, { $set: changes })
         .exec();
       return updated;
@@ -68,17 +74,62 @@ export class GlobalConfigsService {
   async manageProfitMargin(payload: CreateGlobalConfigsDto) {
     try {
       const profitMargin = await this.readProfitMargin();
+      const npmVersion: string = this.configService.version!;
       // If it does not exist, then create it
       if (!profitMargin) {
         const newProfitMargin = await this.createProfitMargin(payload);
-        return newProfitMargin;
+        const {
+          profitMargin: { value, type },
+        } = newProfitMargin;
+        const response: ManageProfitMarginResponse = {
+          version: npmVersion,
+          message: null,
+          error: null,
+          data: {
+            profitMargin: {
+              value,
+              type,
+            },
+          },
+        };
+        return response;
       }
+
       const editPayload: UpdateGlobalConfigsDto = {
         ...payload,
         profitMarginId: profitMargin._id as string,
       };
       const editedProfitMargin = await this.updateProfitMargin(editPayload);
-      return editedProfitMargin;
+      const value = editedProfitMargin?.profitMargin.value;
+      const type = editedProfitMargin?.profitMargin.type;
+      if (!value || !type) {
+        return new BadRequestException('Could not update profit margin');
+      }
+
+      const response: ManageProfitMarginResponse = {
+        version: npmVersion,
+        message: null,
+        error: null,
+        data: {
+          profitMargin: {
+            value,
+            type,
+          },
+        },
+      };
+      return response;
+    } catch (error) {
+      if (error instanceof Error) {
+        throw new BadRequestException(error.message);
+      }
+      throw new BadRequestException('An unknown error occurred');
+    }
+  }
+
+  async getProfitMargin() {
+    try {
+      const profitMargin = await this.readProfitMargin();
+      return profitMargin;
     } catch (error) {
       if (error instanceof Error) {
         throw new BadRequestException(error.message);
