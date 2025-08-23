1ffc2f06399b2ed0e000618a0c40ea03a333216e
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-08-23 11:43:29 -0600

    refactor: enhance ProfitMarginDto validation by adding Min constraint and ApiProperty annotations

--- src/global-configs/dtos/global-configs.dto.ts
+++ src/global-configs/dtos/global-configs.dto.ts
@@ -1,16 +1,34 @@
-import { IsNotEmpty, IsNumber, IsPositive, IsString } from 'class-validator';
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsPositive,
+  IsString,
+  Min,
+} from 'class-validator';
 import { TypeProfitMargin } from '../global-configs.interface';
 import { Type } from 'class-transformer';
-import { PartialType } from '@nestjs/swagger';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
 
 export class ProfitMarginDto {
   @IsNotEmpty()
   @IsNumber()
   @IsPositive()
+  @Min(1, {
+    message: 'The value must be greater or equal to 1',
+  })
+  @ApiProperty({
+    example: 13,
+    description: 'Value of the profit margin',
+  })
   value: number;
 
   @IsNotEmpty()
   @IsString()
+  @ApiProperty({
+    example: 'percentage',
+    description:
+      'Type of the profit margin that could be percentage or absolute',
+  })
   type: TypeProfitMargin;
 }
 
