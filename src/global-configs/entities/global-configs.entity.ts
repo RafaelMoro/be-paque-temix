fd9a2f33475a2ec96bdde1bc2cb8d014717917dd
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-08-23 09:45:17 -0600

    add entity

---
+++ src/global-configs/entities/global-configs.entity.ts
@@ -0,0 +1,17 @@
+import { Prop, Schema } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+import { TypeProfitMargin } from '../global-configs.interface';
+
+export class ProfitMargin {
+  @Prop({ required: true })
+  value: number;
+
+  @Prop({ required: true })
+  type: TypeProfitMargin;
+}
+
+@Schema()
+export class GlobalConfigs extends Document {
+  @Prop({ required: true, type: ProfitMargin })
+  profitMargin: ProfitMargin;
+}
