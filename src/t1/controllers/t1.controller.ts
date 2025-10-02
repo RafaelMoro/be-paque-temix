5eae9c0ef6facbf8dcae39a393de5b687060cde4
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-10-02 06:49:18 -0600

    add controller t1

---
+++ src/t1/controllers/t1.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('t1')
+export class T1Controller {}
--- src/t1/t1.module.ts
+++ src/t1/t1.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { T1Service } from './services/t1.service';
+import { T1Controller } from './controllers/t1.controller';
 
 @Module({
   providers: [T1Service],
   exports: [T1Service],
+  controllers: [T1Controller],
 })
 export class T1Module {}
