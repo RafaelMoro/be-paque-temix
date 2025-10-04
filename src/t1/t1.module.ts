e69e930bffb923d87a09f6ceea3f96b44f805a7b
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-10-04 12:35:27 -0600

    feat: add TokenManagerModule to T1Module for enhanced token management

--- src/t1/t1.module.ts
+++ src/t1/t1.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { T1Service } from './services/t1.service';
 import { GeneralInfoDbModule } from '@/general-info-db/general-info-db.module';
+import { TokenManagerModule } from '@/token-manager/token-manager.module';
 
 @Module({
-  imports: [GeneralInfoDbModule],
+  imports: [GeneralInfoDbModule, TokenManagerModule],
   providers: [T1Service],
   exports: [T1Service],
 })
