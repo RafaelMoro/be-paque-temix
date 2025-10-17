8b6e1bcd8d40a3b5b69f2160563b38e03ce42f91
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-10-17 08:20:06 -0600

    feat: implement PakkeController with JWT guard and create-guide endpoint

--- src/pakke/controllers/pakke.controller.ts
+++ src/pakke/controllers/pakke.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { JwtGuard } from '@/auth/guards/jwt-guard/jwt-guard.guard';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { PakkeService } from '../services/pakke.service';
+import { ApiBearerAuth, ApiOperation } from '@nestjs/swagger';
 
-@Controller('pakke')
-export class PakkeController {}
+@UseGuards(JwtGuard)
+@Controller('pkk')
+export class PakkeController {
+  constructor(private pkkService: PakkeService) {}
+
+  @Post('create-guide')
+  @ApiBearerAuth()
+  @ApiOperation({
+    summary: 'Create a guide for Pkk.',
+  })
+  async createGuide(@Body() payload: any) {
+    return this.pkkService.createGuidePakke(payload);
+  }
+}
