3970f21357524665456eb60d74e718b46d3e415f
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-10-22 17:23:52 -0600

    feat: implement courier services endpoint in GuiaEnviaController

--- src/guia-envia/controllers/guia-envia.controller.ts
+++ src/guia-envia/controllers/guia-envia.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { JwtGuard } from '@/auth/guards/jwt-guard/jwt-guard.guard';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { GuiaEnviaService } from '../services/guia-envia.service';
+import { ApiBearerAuth, ApiOperation } from '@nestjs/swagger';
 
-@Controller('guia-envia')
-export class GuiaEnviaController {}
+@UseGuards(JwtGuard)
+@Controller('ge')
+export class GuiaEnviaController {
+  constructor(private readonly guiaEnviaService: GuiaEnviaService) {}
+
+  @Get('courier-services')
+  @ApiBearerAuth()
+  @ApiOperation({
+    summary: 'Get a list of courier services.',
+  })
+  async getCourierServices() {
+    return this.guiaEnviaService.listServicesGe();
+  }
+}
