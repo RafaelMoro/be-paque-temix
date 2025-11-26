9bdf39e49f0f912acd91fcb92c4e873d724bc9fe
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-11-26 12:42:09 -0600

    feat: create Address entity with required fields

---
+++ src/addresses/entities/addresses.entity.ts
@@ -0,0 +1,31 @@
+import { Prop, Schema } from '@nestjs/mongoose';
+
+@Schema()
+export class Address extends Document {
+  @Prop({ required: true })
+  addressName: string;
+
+  @Prop({ required: true })
+  externalNumber: string;
+
+  @Prop()
+  internalNumber: string;
+
+  @Prop()
+  reference: string;
+
+  @Prop({ required: true })
+  postalCode: string;
+
+  @Prop({ required: true })
+  state: string;
+
+  @Prop({ required: true })
+  city: string[];
+
+  @Prop({ required: true })
+  town: string[];
+
+  @Prop({ required: true })
+  alias: string;
+}
