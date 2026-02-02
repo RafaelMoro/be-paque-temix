85dff92084cde25652780edd8349ae1957dfd041
Rafael Moro <rafael.moro.galindo@gmail.com>
2026-02-02 11:04:13 -0600

    add guides service

--- src/guides/guides.module.ts
+++ src/guides/guides.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { GuidesController } from './controllers/guides.controller';
+import { GuidesService } from './services/guides.service';
 
 @Module({
   controllers: [GuidesController],
+  providers: [GuidesService],
 })
 export class GuidesModule {}
