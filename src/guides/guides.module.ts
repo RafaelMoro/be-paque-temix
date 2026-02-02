d0d0b6bcf8cc27400f05193234907e37ced5c31e
Rafael Moro <rafael.moro.galindo@gmail.com>
2026-02-02 11:09:31 -0600

    Inject GE module and service into guides module

--- src/guides/guides.module.ts
+++ src/guides/guides.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { GuidesController } from './controllers/guides.controller';
 import { GuidesService } from './services/guides.service';
+import { GuiaEnviaModule } from '@/guia-envia/guia-envia.module';
 
 @Module({
+  imports: [GuiaEnviaModule],
   controllers: [GuidesController],
   providers: [GuidesService],
 })
--- src/guides/services/guides.service.ts
+++ src/guides/services/guides.service.ts
@@ -1,4 +1,13 @@
+import { GuiaEnviaService } from '@/guia-envia/services/guia-envia.service';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class GuidesService {}
+export class GuidesService {
+  constructor(private guiaEnviaService: GuiaEnviaService) {}
+
+  getGuides() {
+    return {
+      guide: 'Sample guide',
+    };
+  }
+}
