1c86a6eae2c7e942fb2154840bae42edbca69465
Rafael Moro <rafael.moro.galindo@gmail.com>
2026-02-02 11:22:42 -0600

    implement getGuides endpoint with JWT authentication in GuidesController

--- src/guides/controllers/guides.controller.ts
+++ src/guides/controllers/guides.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { JwtGuard } from '@/auth/guards/jwt-guard/jwt-guard.guard';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { GuidesService } from '../services/guides.service';
 
+@UseGuards(JwtGuard)
 @Controller('guides')
-export class GuidesController {}
+export class GuidesController {
+  constructor(private guidesService: GuidesService) {}
+
+  @Get()
+  async getGuides() {
+    return this.guidesService.getGuides();
+  }
+}
