93179821e22670bd82776780ea0098dab83d53c8
Rafael Moro <rafael.moro.galindo@gmail.com>
2026-02-06 15:23:28 -0600

    feat: add MONGO_CLUSTER_SUFFIX to environment configuration and update database connection logic

--- src/auth/decorators/roles/roles.decorator.ts
+++ src/auth/decorators/roles/roles.decorator.ts
@@ -1,7 +1,6 @@
 import { Role } from '@/users/users.interface';
 import { SetMetadata } from '@nestjs/common';
 
-process.loadEnvFile();
 const roleKey = process.env.ROLE_KEY;
 // Typing the role as (...roles: Role[]) could accept an empty array
 // Typing it this way it means that at least it should have one role
