e2dec0cad52a6e4be3209d339fce7105dacc9be1
Rafael Moro <rafael.moro.galindo@gmail.com>
2026-02-08 22:34:27 -0600

    feat: implement GetQuoteDataResponse interface and update getGuides method to return structured data

--- src/guides/services/guides.service.ts
+++ src/guides/services/guides.service.ts
@@ -1,16 +1,34 @@
+import { Injectable, Inject } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
+
+import config from '@/config';
 import { GuiaEnviaService } from '@/guia-envia/services/guia-envia.service';
-import { Injectable } from '@nestjs/common';
+import { GetQuoteDataResponse } from '../guides.interface';
 
 @Injectable()
 export class GuidesService {
-  constructor(private guiaEnviaService: GuiaEnviaService) {}
+  constructor(
+    private guiaEnviaService: GuiaEnviaService,
+    @Inject(config.KEY) private configService: ConfigType<typeof config>,
+  ) {}
 
-  async getGuides() {
+  async getGuides(): Promise<GetQuoteDataResponse> {
+    const npmVersion: string = this.configService.version!;
     const [GEGuide] = await Promise.allSettled([
       this.guiaEnviaService.getGuides(),
     ]);
+
+    const geQuotesData = GEGuide.status === 'fulfilled' ? GEGuide.value : [];
+
+    const allGuides = [...geQuotesData];
     return {
-      guide: 'Sample guide',
+      version: npmVersion,
+      message: null,
+      messages: [],
+      error: null,
+      data: {
+        guides: allGuides,
+      },
     };
   }
 }
