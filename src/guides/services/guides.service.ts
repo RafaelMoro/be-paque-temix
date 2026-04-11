349db47a2f84b43429b3cc5a4baeb0afa186d7ef
Rafael Moro <rafael.moro.galindo@gmail.com>
2026-04-11 11:56:15 -0600

    feat: integrate PakkeModule and update getGuides method to include Pakke service

--- src/guides/services/guides.service.ts
+++ src/guides/services/guides.service.ts
@@ -6,47 +6,62 @@ import { GuiaEnviaService } from '@/guia-envia/services/guia-envia.service';
 import { GetQuoteDataResponse } from '../guides.interface';
 import { T1Service } from '@/t1/services/t1.service';
 import { T1_USER_NOT_FOUND_ERROR, T1_RETRY_GUIDES } from '@/t1/t1.constants';
+import { PakkeService } from '@/pakke/services/pakke.service';
 
 @Injectable()
 export class GuidesService {
   constructor(
     private guiaEnviaService: GuiaEnviaService,
     private toneService: T1Service,
+    private pakkeService: PakkeService,
     @Inject(config.KEY) private configService: ConfigType<typeof config>,
   ) {}
 
   async getGuides(): Promise<GetQuoteDataResponse> {
     const npmVersion: string = this.configService.version!;
     const messages: string[] = [];
-    const [GEGuide, T1Guide] = await Promise.allSettled([
-      this.guiaEnviaService.getGuides(),
-      this.toneService.retrieveT1Guides(),
-    ]);
+    const [GEGuidesResponse, T1GuidesResponse, PkkGuidesResponse] =
+      await Promise.allSettled([
+        this.guiaEnviaService.getGuides(),
+        this.toneService.retrieveT1Guides(),
+        this.pakkeService.getGuidesPakke({ pageNumber: 1, pageSize: 30 }),
+      ]);
 
-    const geQuotesData = GEGuide.status === 'fulfilled' ? GEGuide.value : [];
-    const t1QuotesData =
-      T1Guide.status === 'fulfilled' ? (T1Guide.value.data ?? []) : [];
+    const geGuidesData =
+      GEGuidesResponse.status === 'fulfilled' ? GEGuidesResponse.value : [];
+    const t1GuidesData =
+      T1GuidesResponse.status === 'fulfilled'
+        ? (T1GuidesResponse.value.data ?? [])
+        : [];
+    const pkkGuidesData =
+      PkkGuidesResponse.status === 'fulfilled' ? PkkGuidesResponse.value : [];
 
     // Handle rejected promises
-    if (GEGuide.status === 'rejected') {
+    if (GEGuidesResponse.status === 'rejected') {
       messages.push('GE failed to get guides');
-      messages.push(`GE Error: ${(GEGuide.reason as Error).message}`);
+      messages.push(`GE Error: ${(GEGuidesResponse.reason as Error).message}`);
     }
-    if (T1Guide.status === 'rejected') {
+    if (T1GuidesResponse.status === 'rejected') {
       messages.push('T1 failed to get guides');
       // Check if error message contains the specific T1 user not found error
-      const errorMessage = (T1Guide.reason as Error).message;
+      const errorMessage = (T1GuidesResponse.reason as Error).message;
       if (errorMessage.includes(T1_USER_NOT_FOUND_ERROR)) {
         messages.push(T1_RETRY_GUIDES);
       }
     }
-
-    // Handle fulfilled cases with messages
-    if (T1Guide.status === 'fulfilled' && T1Guide.value.messages) {
-      messages.push(...T1Guide.value.messages);
+    if (PkkGuidesResponse.status === 'rejected') {
+      messages.push('Pkk failed to get guides');
     }
 
-    const allGuides = [...geQuotesData, ...t1QuotesData];
+    // Handle fulfilled cases with messages
+    if (
+      T1GuidesResponse.status === 'fulfilled' &&
+      T1GuidesResponse.value.messages
+    ) {
+      messages.push(...T1GuidesResponse.value.messages);
+    }
+
+    const allGuides = [...geGuidesData, ...t1GuidesData, ...pkkGuidesData];
     return {
       version: npmVersion,
       message: messages.length > 0 ? messages[0] : null,
