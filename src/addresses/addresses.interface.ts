3d2cb0e7cae3930435e6915d0af16a3ce33979bd
Rafael Moro <rafael.moro.galindo@gmail.com>
2026-05-30 13:44:04 -0600

    feat: enhance address and user models with toObject method and update response types

--- src/addresses/addresses.interface.ts
+++ src/addresses/addresses.interface.ts
@@ -6,11 +6,25 @@ export interface Address extends AddressDoc {
   _id: Types.ObjectId;
 }
 
+export interface AddressData {
+  addressName: string;
+  externalNumber: string;
+  internalNumber?: string;
+  reference?: string;
+  zipcode: string;
+  neighborhood: string;
+  state: string;
+  city: string[];
+  town: string[];
+  alias: string;
+  isGEAddress?: boolean;
+}
+
 export interface CreateAddressResponse
   extends Omit<GeneralResponse, 'data' | 'error'> {
   error: null;
   data: {
-    address: Omit<AddressDoc, '_id' | 'email'>;
+    address: AddressData;
   };
 }
 
@@ -18,7 +32,7 @@ export interface GetAddressesResponse
   extends Omit<GeneralResponse, 'data' | 'error'> {
   error: null;
   data: {
-    addresses: Omit<AddressDoc, '_id' | 'email'>[];
+    addresses: AddressData[];
   };
 }
 
